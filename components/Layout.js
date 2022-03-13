388552dd233e198d81dffab75373636ba7973c05
Roy Roodenburg <hrroodenburg@gmail.com>
2022-03-13 23:49:55 +0100

    logo styling

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import Navigation from "./Navigation/index";
 
 import styles from "../styles/Layout.module.scss";
+import getSvgIcon from "../utils/frontend";
 
 function Layout({ children }) {
   const { menuOpen } = useSelector((state) => state.global);
@@ -14,7 +15,7 @@ function Layout({ children }) {
         <main className={styles["c-main"]}>{children}</main>
         <footer>
           <a href="https://github.com/HRRood/Next_project" className={styles["c-footer__link"]} target="_blank" rel="noopener noreferrer">
-            Made by <strong>Roy Roodenburg</strong> <img src="/images/github.svg" alt="Vercel Logo" className={styles.logo} />
+            Made by <strong>Roy Roodenburg</strong> {getSvgIcon("github")}
           </a>
         </footer>
       </div>
--- components/global/SocialIcons.js
+++ components/global/SocialIcons.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 // Styles
 import styles from "../../styles/components/SocialIcons.module.scss";
 import getSvgIcon from "../../utils/frontend";
 
 const Socialicons = () => {
+  const { menuOpen } = useSelector((state) => state.global);
+
   return (
-    <div className={styles["c-social-icons"]}>
+    <div className={`${styles["c-social-icons"]} ${menuOpen ? styles["c-social-icons--open"] : ""}`}>
       <ul className={styles["c-social-icons__list"]}>
         <li>
           <a href="https://github.com/HRRood" target="_blank" rel="noopener noreferrer">
@@ -14,11 +17,15 @@ const Socialicons = () => {
           </a>
         </li>
         <li>
-          <a href="https://github.com/HRRood" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/roy-roodenburg-444454156/" target="_blank" rel="noopener noreferrer">
             {getSvgIcon("linkedin")}
           </a>
         </li>
-        <li>Instagram</li>
+        <li>
+          <a href="https://www.instagram.com/royrood44/" target="_blank" rel="noopener noreferrer">
+            {getSvgIcon("instagram")}
+          </a>
+        </li>
       </ul>
     </div>
   );
