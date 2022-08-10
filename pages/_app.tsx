7b9921b85e7a1ba41e29c116730a895e98debaaf
Github Main <royroodtje@gmail.com>
2022-08-10 23:54:25 +0200

    Fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import Layout from "../components/Layout";
 
 // STYLEs
 import "../styles/globals.scss";
-import { AppProps } from "next/dist/next-server/lib/router/router";
 import { useState } from "react";
 import { AppContext } from "../contexts/AppContext";
 import Socialicons from "../components/global/SocialIcons";
+import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [menuOpen, setMenuOpen] = useState(false);
